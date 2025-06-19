1bd189f5d96a3634427b019dbc173f25cf3eca8d
Anuj <bpro2855203@gmail.com>
2025-06-19 14:28:06 +0530

    back

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use(cors({
 
 
 // ✅ Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ MongoDB Connected Successfully'))
     .catch((err) => console.error('❌ MongoDB Connection Failed:', err));
 
